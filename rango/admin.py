aa00c08fb6b57fa0bd32ae1304ec2411eb13d256
MediocreMarine <kyle.egland@outlook.com>
2018-11-07 13:31:58 -0600

    Created models for the Rango app

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,9 @@
+#! python
+# Rango App admin.py file
 from django.contrib import admin
+from rango.models import Category
+from rango.models import Page
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Page)
