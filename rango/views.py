ecebd1a94356006e9823e8f97073aaf70e032576
MediocreMarine <kyle.egland@outlook.com>
2018-11-11 12:42:48 -0600

    Modified in order to use models

--- rango/views.py
+++ rango/views.py
@@ -2,12 +2,24 @@
 # Rango app views.py file
 # from django.http import HttpResponse
 from django.shortcuts import render
+# Importing models in order to display
+from rango.models import Category
 
 
 def index(request):
+    # NOTE: Below left in for reference purposes
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy,
+    #                 cupcake!'}
+
+    # Query the database for a list of ALL categories currently stored.  Order
+    # the categories by num likes in descending order.  Retrieve the top 5 only
+    # - or all if less than 5.  Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
     # Return a rendered response to send to the client.  We make use of the
     # shortcut function to make our lives easier.
