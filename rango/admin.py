e9ab47c6e398ef8a0c8c5318109b0447c8024901
MediocreMarine <kyle.egland@outlook.com>
2018-11-11 08:02:41 -0600

    Modified the admin view

--- rango/admin.py
+++ rango/admin.py
@@ -5,5 +5,9 @@ from rango.models import Category
 from rango.models import Page
 
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
