05f4bdc12043c1a40b66a74aa68c4cc95dd230aa
MediocreMarine <kyle.egland@outlook.com>
2018-11-11 19:40:50 -0600

    Mod app view to show categories

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,37 @@
 from django.shortcuts import render
 # Importing models in order to display
 from rango.models import Category
+from rango.models import Page
+
+
+def show_category(request, category_name_slug):
+    # Create a context dictionary which we can pass to the template rendering
+    # engine
+    context_dict = {}
+
+    try:
+        # Can we find a category name slug with the given name?  If we can't,
+        # the .get() method raises a DoesNotExist exception.  So the .get()
+        # method returns one model instance or raises an exception.
+        category = Category.objects.get(slug=category_name_slug)
+
+        # Retrieve all of the associated pages.
+        # NOTE: filter() will return a list of page objects or an empty list
+        pages = Page.objects.filter(category=category)
+
+        # Add the results list to the template context under name pages.
+        context_dict['pages'] = pages
+        # Add the category object from the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # We get here if we didn't find the specified category.  Dont' do
+        # anything - the template will display the "no category" message for us
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    # Go render the resonse and return it to the client
+    return render(request, 'rango/category.html', context_dict)
 
 
 def index(request):
@@ -25,7 +56,7 @@ def index(request):
     # shortcut function to make our lives easier.
     # The render function takes in the user's request, the template, and the
     # context dictionary.
-    return render(request, 'rango/index.html', context=context_dict)
+    return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
