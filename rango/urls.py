97abf2f22ff5d660a06dac4e6dfbe83c680b4641
MediocreMarine <kyle.egland@outlook.com>
2018-11-23 11:57:12 -0600

    Modified to add a base.html template that other rango pages inherit

--- rango/urls.py
+++ rango/urls.py
@@ -3,6 +3,14 @@
 from django.conf.urls import url
 from rango import views
 
+# A 'namespace' can be added to a URL configuration module in order to allow
+# for multiple apps with the same url (I.e. two apps that each have an about
+# page)
+# app_name = 'rango'
+# This would allow for pages to be accessed as follows:
+# <a href="{% url 'rango:about' %}"About</a>
+# The colon seperates the namespace (rango) from the page name
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
